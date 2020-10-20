d01c1935c0e4715ff34781635458f0c2fd188798
Mario Ahumada <ahumadamob@gmail.com>
2020-10-20 12:37:24 -0300

    Chapter 343

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,25 +1,88 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+import { jsx } from '@emotion/core';
 
 import Search from '../ui/Search';
 import Navigation from './Navigation';
+import Button from '../ui/Button';
+
+const HeaderContent = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width:768px){
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--orange);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`;
 
 const Header = () => {
+
+    const isLogged = false;
+
     return ( 
-        <header>
-            <div>
-                <div>
-                    <p>P</p>
+        /** @jsx jsx */
+        <header
+            css={css`
+                border-bottom: 2px solid var(--gray3);
+                padding: 1rem 0;
+            `}
+        >
+            <HeaderContent>
+                <div
+                    css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>                    
                     <Search />
                     <Navigation />
                 </div>
-                <div>
-                    <p>Hola Mario</p>
-                    <button type="submit">Cerrar Sesión</button>
-                    <Link href="/">Iniciar Sesión</Link>
-                    <Link href="/">Crear Cuenta</Link>
+                <div
+                    css={css`
+                        display: flex;
+                        align-items: center;
+                    `}
+                >
+                    {isLogged ? (
+                        <>
+                            <p
+                            css={css`
+                                margin-right: 2rem;
+                            `}
+                            >Hola Mario</p>
+                            <Button bgColor="true">Cerrar Sesión</Button> 
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/login">
+                                <Button bgColor="true">Iniciar Sesión</Button>                        
+                            </Link>
+
+                            <Link href="/create-account">
+                                <Button>Crear Cuenta</Button>
+                            </Link>
+                        </>
+                    )
+                    }
+                                       
+                    
                 </div>
-            </div>
+            </HeaderContent>
         </header>
      );
 }
