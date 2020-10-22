e25def14666ae642e92b4274fb81a2f9b57fe104
Mario Ahumada <ahumadamob@gmail.com>
2020-10-22 10:30:25 -0300

    Chapter 353

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBIgm5ljJslahd1HEGPQxIZXEs7gFYqVnU",
+    authDomain: "product-hunt-8ec87.firebaseapp.com",
+    databaseURL: "https://product-hunt-8ec87.firebaseio.com",
+    projectId: "product-hunt-8ec87",
+    storageBucket: "product-hunt-8ec87.appspot.com",
+    messagingSenderId: "85555641549",
+    appId: "1:85555641549:web:7b295d17020af958173d84"
+  };
+
+export default firebaseConfig;
